ab26f25635aaf69be376d32a5cdbd7e6edc86517
corea <coreashield@gmail.com>
2024-05-03 16:08:24 +0900

    channel toast.make(text)

--- app/src/main/java/com/example/discord/MainActivity.java
+++ app/src/main/java/com/example/discord/MainActivity.java
@@ -7,6 +7,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +23,15 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+
+    public void showCategoryToast(View view) {
+        // Extract category name from TextView's text
+        TextView textView = (TextView) view;
+        String categoryName = textView.getText().toString().substring(0);
+
+        // Show toast with category name
+        Toast.makeText(this, categoryName, Toast.LENGTH_SHORT).show();
     }
 }
